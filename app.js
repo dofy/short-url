cb2e9bf199aeaca3fe0a0416663abdc4fdeb3cde
SevenYu <seven@focohub.com>
2015-01-02 23:27:29 +0800

    add app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express'),
+    app     = express(),
+    config  = require('./lib/config'),
+    db      = require('./lib/db'),
+    url     = require('./lib/models/url'),
+    smart   = require('./lib/smartNumber');
+
+db.connect(config.db, function(err) {
+    if(err) {
+        console.error(err);
+        process.exit(0);
+    } else {
+        smart.chars(config.chars);
+        url.init();
+        console.info('-- service init');
+    }
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use('/_', require('./lib/routers/api'));
+app.use('/', require('./lib/routers/go'));
+
+app.use('*', function(req, res, next) {
+    res.send('Short URL Service');
+});
+
+console.log('=====================================================');
+console.log('==     |-.-|      SHORT URL SERVICE      |-.-|     ==');
+console.log('=====================================================');
+console.log('==        >>>  CTRL+C to Quit Service.  <<<        ==');
+console.log('==    Started at ' + new Date().toUTCString() + '     ==');
+console.log('=====================================================');
+
+app.listen(7777);
+
