753c8b3a6ca2b669c4f301d7f134188ad2093a40
SevenYu <seven@focohub.com>
2015-01-04 20:39:46 +0800

    change fork mode

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
-var spawn = require('child_process').spawn;
+var express = require('express'),
+    app     = express(),
+    config  = require('./lib/config'),
+    db      = require('./lib/db'),
+    url     = require('./lib/models/url'),
+    smart   = require('./lib/smartNumber');
 
-function createChild() {
-    var server = spawn('node', ['short-url.js'], {
-        detached: true,
-        stdio: ['ignore', 'ignore', 'ignore']
-    });
-    server.unref();
-    console.log('foco pid:', server.pid);
+db.connect(config.db, function(err) {
+    if(err) {
+        console.error(err);
+        process.exit(0);
+    } else {
+        smart.chars(config.chars);
+        url.init(smart.str2num('dan'));
+        console.info('-- service init');
+    }
+});
 
-    server.on('error', function(code, signal) {
-        server.kill(signal);
-        server = createChild();
-    });
+app.use(express.static(__dirname + '/public'));
 
-    server.on('exit', function(code, signal) {
-        console.log('Bye :D');
-    });
+app.use('/_', require('./lib/routers/api'));
+app.use('/', require('./lib/routers/go'));
 
-    return server;
-}
+app.use('*', function(req, res, next) {
+    var apiURL = req.protocol + '://' + req.headers.host + '/_';
+    res.send(
+        '|-.-| Short URL Service |-.-|' +
+        '<p>' +
+        '<a href="javascript:void(location.href=\'' + apiURL + '?url=\'+encodeURIComponent(location.href));">&gt;.&lt;</a>' +
+        '</p>'
+        );
+});
 
-createChild();
+console.log('=====================================================');
+console.log('==     |-.-|      SHORT URL SERVICE      |-.-|     ==');
+console.log('=====================================================');
+console.log('==        >>>  CTRL+C to Quit Service.  <<<        ==');
+console.log('==    Started at ' + new Date().toUTCString() + '     ==');
+console.log('=====================================================');
+
+app.listen(7777);
+
